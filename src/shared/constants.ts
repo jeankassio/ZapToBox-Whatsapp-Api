1ba282215018793ac0a0511b9240457b1c79f383
Jean Kassio <jeankassiocheib@gmail.com>
2025-11-12 06:52:43 -0300

    Fixed path of sessions

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -3,7 +3,7 @@ import { ConnectionStatus, InstanceData } from "./types";
 import Instance from "../infra/baileys/services";
 
 export const SessionFolderName = "sessions";
-export const sessionsPath = path.join(__dirname, "..", SessionFolderName);
+export const sessionsPath = path.join(__dirname, "../..", SessionFolderName);
 
 export const instanceConnection: Record<string, InstanceData> = {};
 export const instanceStatus = new Map<string, ConnectionStatus>();
