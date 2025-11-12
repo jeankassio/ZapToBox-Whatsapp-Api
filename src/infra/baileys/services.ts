c0537c72802f5f363e3843eae9080bf9a8bd56f6
Jean Kassio <jeankassiocheib@gmail.com>
2025-11-12 13:16:05 -0300

    Include all events

--- src/infra/baileys/services.ts
+++ src/infra/baileys/services.ts
@@ -90,26 +90,9 @@ export default class Instance{
 
     async instanceEvents(saveCreds: () => Promise<void>){
 
-        this.sock.ev.on("creds.update", saveCreds);
+        this.sock.ev.on("creds.update", saveCreds as (data: BaileysEventMap["creds.update"]) => void);
 
-        this.sock.ev.on("messaging-history.set", async({messages, chats, contacts}: BaileysEventMap['messaging-history.set']) => {
-
-            if(contacts && contacts.length > 0){
-                const contactFiltered = contacts.filter(contact => !!contact.name);
-                trySendWebhook("contacts.set", this.instance, contactFiltered);
-            }
-
-            if(chats && chats.length > 0){
-                trySendWebhook("chats.set", this.instance, chats);
-            }
-
-            if(messages && messages.length > 0){
-                trySendWebhook("messages.set", this.instance, messages);
-            }
-
-        });
-
-        this.sock.ev.on("connection.update", async (update) => {
+        this.sock.ev.on("connection.update", async (update: BaileysEventMap['connection.update']) => {
             const { connection, lastDisconnect, qr } = update;
 
             if (qr) {
@@ -173,37 +156,127 @@ export default class Instance{
             }
         });
 
-        this.sock.ev.on("messages.upsert", async (m) => {
-            await trySendWebhook("messages.upsert", this.instance, [m]);
+        this.sock.ev.on("messaging-history.set", async({messages, chats, contacts}: BaileysEventMap['messaging-history.set']) => {
+
+            if(contacts && contacts.length > 0){
+                const contactFiltered = contacts.filter(contact => !!contact.name);
+                trySendWebhook("contacts.set", this.instance, contactFiltered);
+            }
+
+            if(chats && chats.length > 0){
+                trySendWebhook("chats.set", this.instance, chats);
+            }
+
+            if(messages && messages.length > 0){
+                trySendWebhook("messages.set", this.instance, messages);
+            }
+
         });
 
-        this.sock.ev.on("messages.update", async (m) => {
-            await trySendWebhook("messages.update", this.instance, [m]);
+        this.sock.ev.on("chats.upsert", async (chats: BaileysEventMap['chats.upsert']) => {
+            await trySendWebhook("chats.upsert", this.instance, [chats]);
         });
 
-        this.sock.ev.on("contacts.upsert", async (m) => {
-            await trySendWebhook("contacts.upsert", this.instance, [m]);
+        this.sock.ev.on("chats.update", async (chats: BaileysEventMap['chats.update']) => {
+            await trySendWebhook("chats.update", this.instance, [chats]);
         });
 
-        this.sock.ev.on("chats.upsert", async (m) => {
-            await trySendWebhook("chats.upsert", this.instance, [m]);
+        this.sock.ev.on("chats.delete", async (ids: BaileysEventMap['chats.delete']) => {
+            await trySendWebhook("chats.delete", this.instance, [ids]);
         });
 
-        this.sock.ev.on("presence.update", async (m) => {
-            await trySendWebhook("presence.update", this.instance, [m]);
+        this.sock.ev.on("lid-mapping.update", async (mapping: BaileysEventMap['lid-mapping.update']) => {
+            await trySendWebhook("lid-mapping.update", this.instance, [mapping]);
         });
 
-        this.sock.ev.on("messages.delete", async (m) => {
-            await trySendWebhook("messages.delete", this.instance, [m]);
+        this.sock.ev.on("presence.update", async (presence: BaileysEventMap['presence.update']) => {
+            await trySendWebhook("presence.update", this.instance, [presence]);
         });
 
-        this.sock.ev.on("groups.update", async (m) => {
-            await trySendWebhook("groups.update", this.instance, [m]);
+        this.sock.ev.on("contacts.upsert", async (contacts: BaileysEventMap['contacts.upsert']) => {
+            await trySendWebhook("contacts.upsert", this.instance, [contacts]);
         });
 
-        this?.sock.ev.on("group-participants.update", async (m) => {
-            await trySendWebhook("group-participants.update", this.instance, [m]);
+        this.sock.ev.on("contacts.update", async (contacts: BaileysEventMap['contacts.update']) => {
+            await trySendWebhook("contacts.update", this.instance, [contacts]);
         });
+
+        this.sock.ev.on("messages.upsert", async (messages: BaileysEventMap['messages.upsert']) => {
+            await trySendWebhook("messages.upsert", this.instance, [messages]);
+        });
+
+        this.sock.ev.on("messages.update", async (updates: BaileysEventMap['messages.update']) => {
+            await trySendWebhook("messages.update", this.instance, [updates]);
+        });
+
+        this.sock.ev.on("messages.delete", async (deletes: BaileysEventMap['messages.delete']) => {
+            await trySendWebhook("messages.delete", this.instance, [deletes]);
+        });
+
+        this.sock.ev.on("messages.media-update", async (mediaUpdates: BaileysEventMap['messages.media-update']) => {
+            await trySendWebhook("messages.media-update", this.instance, [mediaUpdates]);
+        });
+
+        this.sock.ev.on("messages.reaction", async (reactions: BaileysEventMap['messages.reaction']) => {
+            await trySendWebhook("messages.reaction", this.instance, [reactions]);
+        });
+
+        this.sock.ev.on("message-receipt.update", async (receipts: BaileysEventMap['message-receipt.update']) => {
+            await trySendWebhook("message-receipt.update", this.instance, [receipts]);
+        });
+
+        this.sock.ev.on("groups.upsert", async (groups: BaileysEventMap['groups.upsert']) => {
+            await trySendWebhook("groups.upsert", this.instance, [groups]);
+        });
+
+        this.sock.ev.on("groups.update", async (groups: BaileysEventMap['groups.update']) => {
+            await trySendWebhook("groups.update", this.instance, [groups]);
+        });
+
+        this.sock.ev.on("group-participants.update", async (update: BaileysEventMap['group-participants.update']) => {
+            await trySendWebhook("group-participants.update", this.instance, [update]);
+        });
+
+        this.sock.ev.on("group.join-request", async (request: BaileysEventMap['group.join-request']) => {
+            await trySendWebhook("group.join-request", this.instance, [request]);
+        });
+
+        this.sock.ev.on("blocklist.set", async (blocklist: BaileysEventMap['blocklist.set']) => {
+            await trySendWebhook("blocklist.set", this.instance, [blocklist]);
+        });
+
+        this.sock.ev.on("blocklist.update", async (update: BaileysEventMap['blocklist.update']) => {
+            await trySendWebhook("blocklist.update", this.instance, [update]);
+        });
+
+        this.sock.ev.on("call", async (calls: BaileysEventMap['call']) => {
+            await trySendWebhook("call", this.instance, [calls]);
+        });
+
+        this.sock.ev.on("labels.edit", async (label: BaileysEventMap['labels.edit']) => {
+            await trySendWebhook("labels.edit", this.instance, [label]);
+        });
+
+        this.sock.ev.on("labels.association", async (assoc: BaileysEventMap['labels.association']) => {
+            await trySendWebhook("labels.association", this.instance, [assoc]);
+        });
+
+        this.sock.ev.on("newsletter.reaction", async (reaction: BaileysEventMap['newsletter.reaction']) => {
+            await trySendWebhook("newsletter.reaction", this.instance, [reaction]);
+        });
+
+        this.sock.ev.on("newsletter.view", async (view: BaileysEventMap['newsletter.view']) => {
+            await trySendWebhook("newsletter.view", this.instance, [view]);
+        });
+
+        this.sock.ev.on("newsletter-participants.update", async (update: BaileysEventMap['newsletter-participants.update']) => {
+            await trySendWebhook("newsletter-participants.update", this.instance, [update]);
+        });
+
+        this.sock.ev.on("newsletter-settings.update", async (update: BaileysEventMap['newsletter-settings.update']) => {
+            await trySendWebhook("newsletter-settings.update", this.instance, [update]);
+        });
+
 
     }
 
