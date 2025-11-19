91b03a26f6981ec4f811bb1bc27a1dd917c72f4c
Jean Kassio <jeankassiocheib@gmail.com>
2025-11-19 00:00:26 -0300

    add new endpoints

--- src/infra/http/controllers/profile.ts
+++ src/infra/http/controllers/profile.ts
@@ -16,41 +16,237 @@ export default class ProfileController {
 
     async onWhatsapp(remoteJid: string){
 
-        const contact = await PrismaConnection.getContactById(this.instance!, remoteJid);
+        try{
 
-        if(contact){
+            const contact = await PrismaConnection.getContactById(this.instance!, remoteJid);
 
-            return {
-                success: true,
-                message: "Contact exists on Whatsapp",
-                data: contact
-            };
+            if(contact){
 
-        }else{
-
-            const results = await this.sock?.onWhatsApp(remoteJid);
-
-            if (results && results.length > 0 && results[0]?.exists) {
-                
-                const result = results[0];
-                
                 return {
                     success: true,
                     message: "Contact exists on Whatsapp",
-                    data: {
-                        id: result?.jid
-                    }
+                    data: contact
                 };
 
             }else{
-                return {
-                    success: false,
-                    error: "Contact dont exists on Whatsapp"
-                };
+
+                const results = await this.sock?.onWhatsApp(remoteJid);
+
+                if (results && results.length > 0 && results[0]?.exists) {
+                    
+                    const result = results[0];
+                    
+                    return {
+                        success: true,
+                        message: "Contact exists on Whatsapp",
+                        data: {
+                            id: result?.jid
+                        }
+                    };
+
+                }else{
+                    return {
+                        success: false,
+                        error: "Contact dont exists on Whatsapp"
+                    };
+                }
+
             }
+
+        }catch(err){
+
+            return {
+                success: false,
+                error: "Error checking number exists on Whatsapp"
+            };
+
+        }
+
+    }
+
+    async fetchStatus(remoteJid: string){
+
+        try{
+
+            const status = await this.sock?.fetchStatus(remoteJid);
+
+            return {
+                success: true,
+                message: "Status fetched",
+                data: {
+                    status
+                }
+            };
+
+        }catch(err){
+
+            return {
+                success: false,
+                error: "Error fetching status"
+            };
 
         }
         
     }
+    
+    async fetchProfilePicture(remoteJid: string){
 
+        try{
+
+            const status = await this.sock?.profilePictureUrl(remoteJid, 'image');
+
+            return {
+                success: true,
+                message: "Successfully",
+                data: {
+                    status
+                }
+            };
+
+        }catch(err){
+
+            return {
+                success: false,
+                error: "Error fetching profile picture"
+            };
+
+        }
+        
+    }
+    
+    async fetchBusinessProfile(remoteJid: string){
+
+        try{
+
+            const profile = await this.sock?.getBusinessProfile(remoteJid);
+
+            return {
+                success: true,
+                message: "Successfully",
+                data: {
+                    profile
+                }
+            };
+
+        }catch(err){
+
+            return {
+                success: false,
+                error: "Error fetching profile Business"
+            };
+
+        }
+        
+    }
+    
+    async presenceSubscribe(remoteJid: string){
+
+        try{
+
+            await this.sock?.presenceSubscribe(remoteJid);
+
+            return {
+                success: true,
+                message: "Successfully"
+            };
+
+        }catch(err){
+
+            return {
+                success: false,
+                error: "Error subscribe presence"
+            };
+
+        }
+        
+    }
+    
+    async profileName(name: string){
+
+        try{
+
+            await this.sock?.updateProfileName(name);
+
+            return {
+                success: true,
+                message: `Successfully, your new name is '${name}'`
+            };
+
+        }catch(err){
+
+            return {
+                success: false,
+                error: "Error changing name"
+            };
+
+        }
+        
+    }
+    
+    async profileStatus(status: string){
+
+        try{
+
+            await this.sock?.updateProfileStatus(status);
+
+            return {
+                success: true,
+                message: `Successfully, your new status is '${status}'`
+            };
+
+        }catch(err){
+
+            return {
+                success: false,
+                error: "Error changing status"
+            };
+
+        }
+        
+    }
+    
+    async updateProfilePicture(remoteJid: string, url: string){
+
+        try{
+
+            await this.sock?.updateProfilePicture(remoteJid, {url});
+
+            return {
+                success: true,
+                message: `Successfully`
+            };
+
+        }catch(err){
+
+            return {
+                success: false,
+                error: "Error changing profile picture"
+            };
+
+        }
+        
+    }
+    
+    async removeProfilePicture(remoteJid: string){
+
+        try{
+
+            await this.sock?.removeProfilePicture(remoteJid);
+
+            return {
+                success: true,
+                message: `Successfully`
+            };
+
+        }catch(err){
+
+            return {
+                success: false,
+                error: "Error removing profile picture"
+            };
+
+        }
+        
+    }
+    
 }
--- src/infra/http/routes/profile.ts
+++ src/infra/http/routes/profile.ts
@@ -17,7 +17,7 @@ export default class ProfileRoutes{
                     return res.status(400).json({ error: "Owner and instanceName are required." });
                 }
 
-                const { id} = req.body;
+                const { id } = req.body;
 
                 if(!id){
                     return res.status(400).json({ error: "Field 'id' is required." });
@@ -25,6 +25,181 @@ export default class ProfileRoutes{
 
                 const mediaController = new ProfileController(owner, instanceName);
                 const result = await mediaController.onWhatsapp(id);
+
+                if(result?.error){
+                    return res.status(500).json(result);
+                }else{
+                    return res.status(200).json(result);
+                }
+
+            })
+            .post("/fetchStatus/:owner/:instanceName", async(req: Request, res: Response) => {
+
+                const owner = req.params.owner;
+                const instanceName = req.params.instanceName;
+
+                if(!owner || !instanceName){
+                    return res.status(400).json({ error: "Owner and instanceName are required." });
+                }
+
+                const { remoteJid } = req.body;
+
+                if(!remoteJid){
+                    return res.status(400).json({ error: "Field 'remoteJid' is required." });
+                }
+
+                const mediaController = new ProfileController(owner, instanceName);
+                const result = await mediaController.fetchStatus(remoteJid);
+
+                if(result?.error){
+                    return res.status(500).json(result);
+                }else{
+                    return res.status(200).json(result);
+                }
+
+            })
+            .post("/fetchProfilePicture/:owner/:instanceName", async(req: Request, res: Response) => {
+
+                const owner = req.params.owner;
+                const instanceName = req.params.instanceName;
+
+                if(!owner || !instanceName){
+                    return res.status(400).json({ error: "Owner and instanceName are required." });
+                }
+
+                const { remoteJid } = req.body;
+
+                if(!remoteJid){
+                    return res.status(400).json({ error: "Field 'remoteJid' is required." });
+                }
+
+                const mediaController = new ProfileController(owner, instanceName);
+                const result = await mediaController.fetchProfilePicture(remoteJid);
+
+                if(result?.error){
+                    return res.status(500).json(result);
+                }else{
+                    return res.status(200).json(result);
+                }
+
+            })
+            .post("/fetchBusinessProfile/:owner/:instanceName", async(req: Request, res: Response) => {
+
+                const owner = req.params.owner;
+                const instanceName = req.params.instanceName;
+
+                if(!owner || !instanceName){
+                    return res.status(400).json({ error: "Owner and instanceName are required." });
+                }
+
+                const { remoteJid } = req.body;
+
+                if(!remoteJid){
+                    return res.status(400).json({ error: "Field 'remoteJid' is required." });
+                }
+
+                const mediaController = new ProfileController(owner, instanceName);
+                const result = await mediaController.fetchBusinessProfile(remoteJid);
+
+                if(result?.error){
+                    return res.status(500).json(result);
+                }else{
+                    return res.status(200).json(result);
+                }
+
+            })
+            .post("/presenceSubscribe/:owner/:instanceName", async(req: Request, res: Response) => {
+
+                const owner = req.params.owner;
+                const instanceName = req.params.instanceName;
+
+                if(!owner || !instanceName){
+                    return res.status(400).json({ error: "Owner and instanceName are required." });
+                }
+
+                const { remoteJid } = req.body;
+
+                if(!remoteJid){
+                    return res.status(400).json({ error: "Field 'remoteJid' is required." });
+                }
+
+                const mediaController = new ProfileController(owner, instanceName);
+                const result = await mediaController.presenceSubscribe(remoteJid);
+
+                if(result?.error){
+                    return res.status(500).json(result);
+                }else{
+                    return res.status(200).json(result);
+                }
+
+            })
+            .post("/profileName/:owner/:instanceName", async(req: Request, res: Response) => {
+
+                const owner = req.params.owner;
+                const instanceName = req.params.instanceName;
+
+                if(!owner || !instanceName){
+                    return res.status(400).json({ error: "Owner and instanceName are required." });
+                }
+
+                const { name } = req.body;
+
+                if(!name){
+                    return res.status(400).json({ error: "Field 'name' is required." });
+                }
+
+                const mediaController = new ProfileController(owner, instanceName);
+                const result = await mediaController.profileName(name);
+
+                if(result?.error){
+                    return res.status(500).json(result);
+                }else{
+                    return res.status(200).json(result);
+                }
+
+            })
+            .post("/profileStatus/:owner/:instanceName", async(req: Request, res: Response) => {
+
+                const owner = req.params.owner;
+                const instanceName = req.params.instanceName;
+
+                if(!owner || !instanceName){
+                    return res.status(400).json({ error: "Owner and instanceName are required." });
+                }
+
+                const { status } = req.body;
+
+                if(!status){
+                    return res.status(400).json({ error: "Field 'status' is required." });
+                }
+
+                const mediaController = new ProfileController(owner, instanceName);
+                const result = await mediaController.profileStatus(status);
+
+                if(result?.error){
+                    return res.status(500).json(result);
+                }else{
+                    return res.status(200).json(result);
+                }
+
+            })
+            .post("/profilePicture/:owner/:instanceName", async(req: Request, res: Response) => {
+
+                const owner = req.params.owner;
+                const instanceName = req.params.instanceName;
+
+                if(!owner || !instanceName){
+                    return res.status(400).json({ error: "Owner and instanceName are required." });
+                }
+
+                const { jid, url } = req.body;
+
+                if(!jid){
+                    return res.status(400).json({ error: "Fields 'jid', 'url' and 'active' is required." });
+                }
+
+                const mediaController = new ProfileController(owner, instanceName);
+                const result = await (url ? mediaController.updateProfilePicture(jid, url) : mediaController.removeProfilePicture(jid));
 
                 if(result?.error){
                     return res.status(500).json(result);
