1fc6addb2a3fcb38d8f3a31fcde68fe1788ddca0
Jean Kassio <jeankassiocheib@gmail.com>
2025-11-23 03:32:13 -0300

    debug

--- src/infra/baileys/services.ts
+++ src/infra/baileys/services.ts
@@ -302,7 +302,7 @@ export default class Instance{
         this.sock.ev.on("messages.upsert", async (messages: BaileysEventMap['messages.upsert']) => {
             this.sock.sendPresenceUpdate('unavailable');
             PrismaConnection.saveManyMessages(`${this.instance.owner}_${this.instance.instanceName}`, messages.messages);
-            await trySendWebhook("messages.upsert", this.instance, [messages.messages]);
+            await trySendWebhook("messages.upsert", this.instance, [messages]);
         });
 
         this.sock.ev.on("messages.update", async (updates: BaileysEventMap['messages.update']) => {
